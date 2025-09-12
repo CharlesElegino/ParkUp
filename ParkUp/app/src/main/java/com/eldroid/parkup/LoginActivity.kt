4fbcdf4b5626573d125740abe78b4536022c0e4d
Charles Elejean Elegino <153287190+CharlesElegino@users.noreply.github.com>
2025-09-13 03:22:40 +0800

    1st Commit

---
+++ ParkUp/app/src/main/java/com/eldroid/parkup/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.eldroid.parkup
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.login_button)
+        loginBtn.setOnClickListener{
+
+        }
+    }
+}
